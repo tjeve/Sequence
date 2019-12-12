a56252567c9e2c6b8a4753ad8f038be3817c212d
Terrence Eve <tijayeve@gmail.com>
2019-12-12 17:43:53 -0600

    Each card in the grid now has a unique key

--- src/cards.js
+++ src/cards.js
@@ -18,12 +18,17 @@ const makeGameBoardDeck = () => {
        
         for (let x = 0; x < suits.length; x++) {
             for (let y = 0; y < values.length; y++) {
-              card = {key: null, suit: suits[x], val: values[y], type: 'card'};
+              card = {
+                  key: null, 
+                  suit: suits[x], 
+                  val: values[y], 
+                  type: 'card'
+                };
               cardDeck.push(card);
             }
           };  
     }
-
+    
     let doubleDeck = cardDeck.concat(cardDeck)
     console.assert(doubleDeck.length === 96, "DoubleDeck does not have 96 cards")
         return doubleDeck
@@ -65,7 +70,7 @@ class GameBoardDeck extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-          cardDeck: makeGameBoardDeck()
+          cardDeck: this.shuffleCards(makeGameBoardDeck()),
       };
     }
     
@@ -86,17 +91,13 @@ class GameBoardDeck extends React.Component {
 
     handleShuffleCards = () => { //Changes the state of cardDeck to the shuffled hand of cards
         let shuffledCards = this.shuffleCards(this.state.cardDeck)
-        // console.log(shuffledCards)
 
         this.setState({
             cardDeck: shuffledCards
         })
 
     }
-    // handleGameBoard = () => {
-    //     let shuffledGameBoard = this.BuildGameBoard(createRenderedGameBoard(this.state.cardDeck))
-    //     console.log(shuffledGameBoard)
-    // }
+
     render() {
       return (  
         <div>
@@ -130,20 +131,30 @@ class GameBoardDeck extends React.Component {
         return <Row key={index}>{ singleRowOfCards }</Row>
     }
 
-    let freeSpaceInfo = {key: null, suit: 'FREE SPACE ⭐', val: null, type: 'freeSpace'}
-    let freeSpaceCard = MakeCard(freeSpaceInfo)
-    let freeSpace = makeCol(freeSpaceCard)
+    let freeSpaceInfo = [
+        {key: null, suit: 'FREE SPACE ⭐', val: null, type: 'freeSpace'},
+        {key: null, suit: 'FREE SPACE ⭐', val: null, type: 'freeSpace'},
+        {key: null, suit: 'FREE SPACE ⭐', val: null, type: 'freeSpace'},
+        {key: null, suit: 'FREE SPACE ⭐', val: null, type: 'freeSpace'}
+    ]
+    
+    let freeSpaceCard = freeSpaceInfo.map((card, index) => {return <MakeCard key ={index + 100} suit={card.suit} value={card.val}/>})
+    // console.log(freeSpaceCard)
+    // let freeSpaceCard = <MakeCard key={index} suit={freeSpaceInfo.suit} value={freeSpaceInfo.val}/>
+    // let createCardsHTML = cardDeck.map((card, index) => {return <MakeCard key={index} suit={card.suit} value={card.val}/>} )
+    let freeSpace = freeSpaceCard.map((card, index) => <Col key={index + 100}>{card}</Col>)
 
-    const createGrid = (CardArray) => {
+    const createGrid = (CardArray, index) => {
         let gridContainer = [[],[],[],[],[],[],[],[],[],[]]
-        
+        let freeSpaceCounter = 0
         let cardCounter = 0 // counts the number of cards added to the gameboard array
         for (let rowIdx = 0; rowIdx < gridContainer.length; rowIdx++) { // for every row...
             for (let spaceIdx = 0; spaceIdx < 10; spaceIdx++) { // in every space...
                 let row = gridContainer[rowIdx] // define a row as this...
                 // check the grid location and if the position is (0,0), (0,9), (9,0), or (9,9)...
                 if ((spaceIdx === 0 || spaceIdx === 9) && (rowIdx === 0 || rowIdx === 9)) {
-                    row.push(freeSpace) // push a freespace to that location
+                    row.push(freeSpace[freeSpaceCounter]) // push a freespace to that location
+                    freeSpaceCounter++
                 } else {    // otherwise...
                     row.push(CardArray[cardCounter]) // push a card from the cardArray (with length of 96)
                     cardCounter++;  // then increase the cardCounter to move to the next card of the cardArray
@@ -151,7 +162,7 @@ class GameBoardDeck extends React.Component {
                 console.assert(gridContainer.length === 10, "Grid container should contain 10 rows.")
             }
         }
-        
+        console.log(gridContainer)
         console.assert(gridContainer.length === 10, "Make sure all 100 cards have been pushed to the gridContainer")
         return gridContainer
     }
